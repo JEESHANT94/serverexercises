fe512e245b52caba1c70111d2fad3ef77bb1e3d8
JeeshantPatel <73455314+JEESHANT94@users.noreply.github.com>
2024-09-30 00:00:30 -0400

    Update ExpenseCategory.java

--- src/main/java/com/info5059/serverexercises/expense/ExpenseCategory.java
+++ src/main/java/com/info5059/serverexercises/expense/ExpenseCategory.java
@@ -3,9 +3,7 @@ import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-/**
-* Entity class to keep track of Expense types
-*/
+
 @Entity
 @Data
 @RequiredArgsConstructor
